97fbc27ce2e5c9fa13746af1ef4cd5c1fbb2264b
Jonas Almeida <jalmeida@mathbiol.org>
2025-08-19 17:06:28 -0400

    tbc

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ console.log(`usai index.js at ${Date()}`);
     // anonymous asynchronous execution, note use of command line module, cli
     const usai = await import('../usai/usai.mjs')
     const cli = await import('../cli/cli.mjs')
-    cli.msg(`usai methods: ${Object.getOwnPropertyNames(usai)}`,'usaimsg')
+    usai.msg(`usai methods: ${Object.getOwnPropertyNames(usai)}`,'usaimsg')
 })();
