872f687bcf6de205bcfc3cc52113bd08fb84a05a
Jonas Almeida <jalmeida@mathbiol.org>
2025-08-19 12:39:35 -0400

    ini

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`usai index.js at ${Date()}`);
+
+(async function(){
+    // anonymous asynchronous execution, note use of command line module, cli
+    const usai = await import('../usai/usai.mjs')
+    const cli = await import('../cli/cli.mjs')
+    cli.msg(`usai methods: ${Object.getOwnPropertyNames(usai)}`,'usaimsg')
+})();
---
+++ usai.mjs
@@ -0,0 +1,3 @@
+const hello = 'hello usai world'
+
+export{hello}
